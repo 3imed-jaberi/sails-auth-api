8182b552b492988000ae2668bc3313c56c59bd10
unknown <imed_jebari@hotmail.fr>
2019-05-07 03:01:42 +0200

    def all private - auth - routes ..

--- config/policies.js
+++ config/policies.js
@@ -18,5 +18,8 @@ module.exports.policies = {
   ***************************************************************************/
 
   // '*': true,
+  UserController: {
+    'profile': 'isLogged'
+  }
 
 };
